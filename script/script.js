2370f9fff78ef0b4cbc38468d48337265bc5ab71
Alinur Mammadzada <138376201+m11jr@users.noreply.github.com>
2024-10-06 17:58:01 +0300

    S

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function openSide(){
+var sideBar = document.getElementById("SideBar");
+sideBar.style.visibility="visible";
+sideBar.style.backdropFilter = "blur(20px)"
+}
+function closeSide(){
+  var sideBar = document.getElementById("SideBar");
+  sideBar.style.visibility="hidden";
+
+}
