c04cc481ed7a054757e9567207bc9d3aa213c3b1
Alinur Mammadzada <138376201+m11jr@users.noreply.github.com>
2024-10-06 22:58:43 +0300

    S

--- script/script.js
+++ script/script.js
@@ -8,3 +8,30 @@ function closeSide(){
   sideBar.style.visibility="hidden";
 
 }
+
+
+
+function changeBackground(image) {
+  // Change background image of the MovieTab section
+  document.getElementById('Movies').style.backgroundImage = `url('assests/${image}')`;
+}
+
+function resetBackground() {
+  // Reset the background image to the original
+  document.getElementById('Movies').style.backgroundImage = "url('assests/marvel-avengers.gif')";
+}
+
+// function backgroundChanger(){
+// var oldback = document.querySelector('.movie1 img');
+// oldback.style.backgroundImage="url(..\assests\DeadpoolWolverineBack.gif)";
+//
+// }
+//
+// function backgroundReset(){
+//
+// }
+//
+//
+// document.querySelector('.movie1 img').addEventListener('mouseout', function() {
+//   document.querySelector('.MovieTab').style.backgroundImage = "url(..\assests\marvel-avengers.gif)";
+// });
